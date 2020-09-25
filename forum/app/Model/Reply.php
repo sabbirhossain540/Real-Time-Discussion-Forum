f173cfbd0acb01b1f2b56f65b0c2ecdb7435b3ae
Sabbir Hossain <m.sabbirhossain308@gmail.com>
2020-09-25 11:15:00 +0600

    relatioship

--- forum/app/Model/Reply.php
+++ forum/app/Model/Reply.php
@@ -3,8 +3,23 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Model\Question;
+use App\Model\Like;
+use App\User;
+
 
 class Reply extends Model
 {
-    //
+    public function question(){
+    	return $this->belongsTo(Question::class);
+    }
+
+
+    public function user(){
+    	return $this->belongsTo(User::class);
+    }
+
+    public function likes(){
+    	return $this->hasMany(Like::class);
+    }
 }
