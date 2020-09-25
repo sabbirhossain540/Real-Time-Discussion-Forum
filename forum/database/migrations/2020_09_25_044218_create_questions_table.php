8a668389728985e2e1ad611af48bc8de581a3f05
Sabbir Hossain <m.sabbirhossain308@gmail.com>
2020-09-25 10:48:41 +0600

    question migration

--- forum/database/migrations/2020_09_25_044218_create_questions_table.php
+++ forum/database/migrations/2020_09_25_044218_create_questions_table.php
@@ -14,7 +14,12 @@ class CreateQuestionsTable extends Migration
     public function up()
     {
         Schema::create('questions', function (Blueprint $table) {
-            $table->id();
+            $table->id('id');
+            $table->string('title');
+            $table->string('slug');
+            $table->text('body');
+            $table->integer('category_id')->unsigned();
+            $table->integer('user_id')->unsigned();
             $table->timestamps();
         });
     }
