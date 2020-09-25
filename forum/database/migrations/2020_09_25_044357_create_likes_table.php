cb5b4f2414db61c262ce1d5b59e897801e3b8c4f
Sabbir Hossain <m.sabbirhossain308@gmail.com>
2020-09-25 10:57:18 +0600

    category and like migration

--- forum/database/migrations/2020_09_25_044357_create_likes_table.php
+++ forum/database/migrations/2020_09_25_044357_create_likes_table.php
@@ -14,7 +14,9 @@ class CreateLikesTable extends Migration
     public function up()
     {
         Schema::create('likes', function (Blueprint $table) {
-            $table->id();
+            $table->id('id');
+            $table->integer('reply_id');
+            $table->integer('user_id');
             $table->timestamps();
         });
     }
