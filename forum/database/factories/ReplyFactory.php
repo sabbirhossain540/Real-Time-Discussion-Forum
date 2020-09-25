258f83a9aa284d01110e31ab4c70e262b845b87b
Sabbir Hossain <m.sabbirhossain308@gmail.com>
2020-09-25 17:35:10 +0600

    factory done

--- forum/database/factories/ReplyFactory.php
+++ forum/database/factories/ReplyFactory.php
@@ -4,9 +4,17 @@
 
 use App\Model\Reply;
 use Faker\Generator as Faker;
+use App\Model\Question;
+use App\User;
 
 $factory->define(Reply::class, function (Faker $faker) {
     return [
-        //
+        'body' => $faker->text,
+        'question_id' => function(){
+        	return Question::all()->random();
+        },
+        'user_id' => function(){
+        	return User::all()->random();
+        }
     ];
 });
