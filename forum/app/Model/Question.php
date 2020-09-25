63b763e18d87438f42380012fd56d79d25f83ecc
Sabbir Hossain <m.sabbirhossain308@gmail.com>
2020-09-25 11:08:59 +0600

    ds

--- forum/app/Model/Question.php
+++ forum/app/Model/Question.php
@@ -3,8 +3,24 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
+use App\User;
+use App\Model\Reply;
+use App\Model\Category;
 
 class Question extends Model
 {
-    //
+    public function user(){
+    	return $this->belongsTo(User::class);
+    }
+
+    public function replies(){
+    	return $this->hasMany(Reply::class);
+    }
+
+    public function category(){
+    	return $this->belongsTo(Category::class);
+    }
+
+
+
 }
