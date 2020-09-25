01961af1df403bc130809474afa7116bf34185ee
Sabbir Hossain <m.sabbirhossain308@gmail.com>
2020-09-25 10:54:10 +0600

    reply table migration

--- forum/database/migrations/2020_09_25_044310_create_replies_table.php
+++ forum/database/migrations/2020_09_25_044310_create_replies_table.php
@@ -14,7 +14,11 @@ class CreateRepliesTable extends Migration
     public function up()
     {
         Schema::create('replies', function (Blueprint $table) {
-            $table->id();
+            $table->id('id');
+            $table->text('body');
+            $table->integer('question_id')->unsigned();
+            $table->integer('user_id');
+            $table->foreign('question_id')->references('id')->on('questions')->onDelete('cascade');
             $table->timestamps();
         });
     }
