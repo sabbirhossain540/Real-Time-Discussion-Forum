5cf1563522f6d799bd2eb653963ecc67f614576b
Sabbir Hossain <m.sabbirhossain308@gmail.com>
2020-09-26 11:36:29 +0600

    ds

--- forum/app/User.php
+++ forum/app/User.php
@@ -5,9 +5,10 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 use App\Model\Question;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use Notifiable;
 
@@ -41,5 +42,25 @@ class User extends Authenticatable
 
     public function question(){
         return $this->hasMany(Question::class);
+    }
+
+     /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
     }
 }
--- forum/routes/api.php
+++ forum/routes/api.php
@@ -1,5 +1,21 @@
 <?php
 
+Route::group([
+
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+
+});
+
+
+
 Route::apiResource('/question','QuestionController');
 Route::apiResource('/category','CategoryController');
 Route::apiResource('/question/{question}/reply','ReplyController');
