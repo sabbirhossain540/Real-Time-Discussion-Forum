cc14a077b0d3cadb6972743a034892583e27b33f
Sabbir Hossain <m.sabbirhossain308@gmail.com>
2020-09-26 17:28:49 +0600

    ds

--- forum/app/Http/Controllers/AuthController.php
+++ forum/app/Http/Controllers/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Controller;
+use App\User;
 
 class AuthController extends Controller
 {
@@ -15,7 +16,7 @@ class AuthController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth:api', ['except' => ['login']]);
+        $this->middleware('auth:api', ['except' => ['login', 'signup']]);
     }
 
     /**
@@ -32,6 +33,13 @@ class AuthController extends Controller
         }
 
         return $this->respondWithToken($token);
+    }
+
+
+    public function signup(Request $request){
+        
+        User::create($request->all());
+        return $this->login($request);
     }
 
     /**
