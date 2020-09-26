3b50b383feb745e5904c7079a7da02e825db1752
Sabbir Hossain <m.sabbirhossain308@gmail.com>
2020-09-26 17:50:10 +0600

    ds

--- forum/app/Exceptions/Handler.php
+++ forum/app/Exceptions/Handler.php
@@ -4,6 +4,7 @@ namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use Tymon\JWTAuth\Exceptions\JWTException;
 
 class Handler extends ExceptionHandler
 {
@@ -50,6 +51,9 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Throwable $exception)
     {
+        if($exception instanceof JWTException){
+            return response(['error'=>'Token is not provided']);
+        }
         return parent::render($request, $exception);
     }
 }
